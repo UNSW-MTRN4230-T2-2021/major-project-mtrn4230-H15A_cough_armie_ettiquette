94e9cb999d17959af5f9c8c6477ef5651c9e620a
jovannitjuandi <48846832+jovannitjuandi@users.noreply.github.com>
2021-08-12 17:07:43 +1000

    Jovanni/controller (#22)
    * Implemented a controller for testing, set up package
    
    * Implemented Board State and its required message and service files
    
    * Added dummy Image Processor file for service testing
    
    * Added TODO comments
    
    * Altered nodes to fit new ros node diagram
    
    * Add Functions to Implement in gamecontroller
    
    * Add ControllerMessage and functions to cpp
    
    * Published Message Controller For Test
    
    * determine Current Player
    
    * Check for set winner
    
    * Check for overall game winner
    
    * Add message type for UI requests
    
    Co-authored-by: Snouzle <z5209697@ad.unsw.edu.au>

--- project/include/GameController.hpp
+++ project/include/GameController.hpp
@@ -2,8 +2,11 @@
 #define CONTROL_H
 
 #include <ros/ros.h>
+#include <cmath>
 
 #include "project/BoardInfo.h"
+#include "project/ControllerMessage.h"
+#include "project/PiecePosition.h"
 #include "BoardState.hpp"
 
 class GameController {
@@ -11,36 +14,44 @@ public:
     enum Player {
             OP = 1, // indicates the opponent
             AI = -1,// indicates the computer
-            NA = 0  // indicates an undefined
+            NA = 0, // indicates an undefined
+            DR = 2  // indicates one set draw
         };
 
     enum DifficultyLevel {
-            Easy = 0,
+            Easy = -1,
             Hard = 1,
-            Null = -1,
+            Null = 0,
     };
 
     static const int TOTAL_STAT = 3;
+    ros::NodeHandle n; /* This has to be public for each class */
 
 private:
     BoardState mState;
     ros::Subscriber mImageSub;
-    std::array<int, TOTAL_STAT> WinnerArray;
+    std::array<GameController::Player, TOTAL_STAT> WinnerArray;
+    project::ControllerMessage controllerStatus;
+    ros::Publisher controllerPublisher;
+
     int SetCount;
-    int CurrentPlayer;
-    int DifficultyLevel;
+    Player CurrentPlayer;
+    DifficultyLevel SelectedDifficulty;
     int TimeLimit;
+    bool GameActive;
+    bool SetStarted;
     
 public:
-    GameController(ros::NodeHandle &n);
-
     void showBoardState() { mState.showBoardState(); }
+
+    GameController();
 
     void imageCallBack(const project::BoardInfo::ConstPtr &msg);
 
     void saveBoardState(BoardState &state);
 
-    void determineCurrentPlayer(Player PreviousPlayer);
+    void determineCurrentPlayer();
+
 
     void indicateSetWinner();
 
--- project/src/GameController.cpp
+++ project/src/GameController.cpp
@@ -1,46 +1,164 @@
 #include "GameController.hpp"
 
-GameController::GameController(ros::NodeHandle &n):
-    mImageSub{n.subscribe("processed_image", 100, &GameController::imageCallBack, this)},
+GameController::GameController():
     SetCount{0},
-    CurrentPlayer{0},
-    DifficultyLevel{-1},
-    TimeLimit{120}
+    CurrentPlayer{NA},
+    SelectedDifficulty{Null},
+    TimeLimit{120},
+    GameActive{false},
+    SetStarted{false}
 {
+    controllerPublisher = n.advertise<project::ControllerMessage>("controller_message", 100);
+    mImageSub = n.subscribe("processed_image", 100, &GameController::imageCallBack, this);
+    
+    for (int i = 0; i < TOTAL_STAT; i++) {
+        controllerStatus.setWinners.push_back(NA);
+    }
 }
 
 
 void GameController::imageCallBack(const project::BoardInfo::ConstPtr &msg) {
     BoardState newBoard(msg->board);
-
     newBoard.showBoardState();
 }
-
-
-// BoardState GameController::getNewBoardState() {
-//     project::ImageService srv;
-//     srv.request.flag = 0;
-
-//     if (mImageClient.call(srv)) {
-//         BoardState newBoard{srv.response.board};
-//         // TODO: Process srv.response.pieces (remaining pieces)
-//         return newBoard;
-//     } else {
-//         ROS_ERROR("Failed to call Image Service");
-//     }
-// }
 
 void GameController::saveBoardState(BoardState &state) {
     mState.setBoardState(state);
 }
 
+void GameController::determineCurrentPlayer() {
+    if (SetStarted) {
+        if (controllerStatus.playerId == AI) {
+            controllerStatus.playerId = OP;
+        } 
+        else if (controllerStatus.playerId == OP) {
+            controllerStatus.playerId = AI;
+        }
+        else {
+            if (SelectedDifficulty == GameController::Hard) {
+                controllerStatus.playerId = AI;;
+            }
+            else if (SelectedDifficulty == GameController::Easy) {
+                controllerStatus.playerId = OP;
+            }
+            else {
+                // TODO: Throw Exception --> Request for a difficulty level input
+            }
+        }
+    }
+    
+    GameController::controllerPublisher.publish(GameController::controllerStatus);
+}
+
+void GameController::indicateSetWinner() {
+    /* TRIADS: ROWs, COLs, when row == col */
+    // TODO: double check the letters used and who the players are, then update controllerStatus.setWinners[SetCount] = OP or AI
+    
+    BoardState::Board board = mState.getBoard();
+    bool winnerFound = false;
+    
+    // Check for all rows
+    for (int row = 0; row < BoardState::BOARD_SIZE; row++) {
+        if (board[row][0] == board[row][1] && board[row][1] == board[row][2] && board[row][0] != ' ') {
+            controllerStatus.setWinners[SetCount] = board[row][0];
+            winnerFound = true;
+        }
+    }
+    
+    // Check for all columns
+    for (int col = 0; col < BoardState::BOARD_SIZE; col++) {
+        if (board[0][col] == board[1][col] && board[1][col] == board[2][col] && board[0][col] != ' ') {
+            controllerStatus.setWinners[SetCount] = board[0][col];
+            winnerFound = true;
+        }
+    }
+    
+    // Check for diagonals
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+        controllerStatus.setWinners[SetCount] = board[0][0];
+        winnerFound = true;
+    }
+    
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ' ') {
+        controllerStatus.setWinners[SetCount] = board[1][1];
+        winnerFound = true;
+    }
+    
+    // Check for draw
+    if (!winnerFound) {
+        bool isFull = true;
+        for (int row = 0; row < BoardState::BOARD_SIZE; row++) {
+            for (int col = 0; col < BoardState::BOARD_SIZE; col++) { 
+                if (board[row][col] == ' ') {
+                    isFull = false;
+                }
+            }
+        }
+        
+        if (isFull) {
+            controllerStatus.setWinners[SetCount] = DR;
+        }
+    }
+    
+    GameController::controllerPublisher.publish(GameController::controllerStatus);
+}
+
+bool isOP(int p) { return (p == GameController::OP); }
+
+bool isAI(int p) { return (p == GameController::AI); }
+
+void GameController::determineGameWinner() {
+    // TODO: Update so that it works as such: this only gets called when a winner of a set is found this increment setcount after
+    if (SetCount < 2) {
+        controllerStatus.gameWinner = NA;
+    }
+    
+    int OPwincount = count_if(controllerStatus.setWinners.begin(), controllerStatus.setWinners.end(), isOP);
+    int AIwincount = count_if(controllerStatus.setWinners.begin(), controllerStatus.setWinners.end(), isAI);
+    
+    if (std::abs(OPwincount - AIwincount) > 1) {
+        if (OPwincount < AIwincount) {
+            controllerStatus.gameWinner = AI;
+        }
+        else if (AIwincount < OPwincount) {
+            controllerStatus.gameWinner = OP;
+        }
+    }
+    
+    if (SetCount == 2 && controllerStatus.gameWinner == NA) {
+        controllerStatus.gameWinner = DR;
+    }
+}
+
+void decideMove() { // RETURN TYPE DEPENDS ON GAZEBO CONTROLLER
+
+}
+
+bool validateMove(const BoardState currentInput); // INCLUDES CHECKING FOR TIME AND 
+
+void throwViolation(); // RETURN TYPE DEPENDS ON USER INTERFACE
+
+void endGame(); // announce winner and clear board and ask if user wants to play again
+
+
 int main(int argc, char **argv) {
-
+    
+    std::cout << "Controller Initiating...." << std::endl;
     ros::init(argc, argv, "game_controller");
-    ros::NodeHandle n;
-
-    // Random code for testing
-    GameController c(n);
-
+    
+    std::cout << "GameController Readying...." << std::endl;
+    GameController firstGame;
+    
+    std::cout << "Game Begin!" << std::endl;
+    
+    while(ros::ok()) {
+        firstGame.determineCurrentPlayer();
+        firstGame.indicateSetWinner();
+        firstGame.determineGameWinner();
+        ros::Duration(3).sleep();
+    }
+    
+    std::cout << "Game Finished!" << std::endl;
+    std::cout << "Thanks for playing!" << std::endl;
     ros::spin();
 }
