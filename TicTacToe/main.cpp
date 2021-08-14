953beb1ca34d6a5f5c5462d622210c9e6c38e359
IraDove <you@example.com>
2021-08-14 11:32:39 +1000

    Updated UI with publish/subscribe capacity and functionality for 'End Turn' button. Data output to topic msgsToController: - Integer 0 at start of program - Integer 3 after pressing 'End Turn'

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -1,60 +1,20 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-
-#include <thread>
-
 #include "mainwindow.h"
 #include <QApplication>
+#include <thread>
 
-int pubThread(int argc, char **argv)
-{
-  ros::init(argc, argv, "publisher");
-  ros::NodeHandle nh;
-
-  ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
-
-  ros::Rate loop_rate(10);
-  while (ros::ok())
-  {
-    std_msgs::String msg;
-    msg.data = "hello world";
-
-    chatter_pub.publish(msg);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-  }
-
-  return 0;
+void thread() {
+    ros::spin();
 }
-
-
-void subThreadCallback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
-}
-
-int subThread(int argc, char **argv)
-{
-  ros::init(argc, argv, "subscriber");
-  ros::NodeHandle nh;
-
-  ros::Subscriber sub = nh.subscribe("chatter", 1000, subThreadCallback);
-
-  ros::spin();
-
-  return 0;
-}
-
 
 int main(int argc, char *argv[])
 {
-    std::thread firstthread(pubThread, argc, argv);
-    std::thread secondthread(subThread, argc, argv);
+    ros::init(argc, argv, "uiNode");
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    std::thread t(thread);
 
     return a.exec();
 }
